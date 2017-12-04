f37089030a6ff7d5446c452a51adf0b5421268e5
gbri140 <glb3@rice.edu>
2017-12-03 20:02:37 -0600

    Update Inclass21.m

--- Inclass21.m
+++ Inclass21.m
@@ -1,5 +1,11 @@
 %In class 21
 
+%GB comments
+1 100
+2 100
+overall 100
+
+
 % 1. Consider the system we discussed in class for a gene which is produced
 % and degraded: dx/dt = k - d*x. Now consider that this gene turns on a
 % second gene y, which also degrades so that its equation is 
